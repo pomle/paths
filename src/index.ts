b7fff7b831584ac731a04ca9c5af310cc25f26e2
Pontus Alexander <pontus.alexander@gmail.com>
2021-09-05 10:17:45 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import type { ParamCodec, PathCodec } from './path';
-export type { ParamCodec, PathCodec };
+import type { Path, ParamCodec, PathCodec } from './path';
+export type { Path, ParamCodec, PathCodec };
 
 import { createCodec } from './codec';
 import { createPath } from './path';
-
 export { createCodec, createPath };
