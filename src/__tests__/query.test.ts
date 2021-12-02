defacacb4ce2e44597deb1b074ab4a592e407410
Pontus Persson <pontus@cur8.co>
2021-12-02 09:05:20 +0100

    Add query handler

---
+++ src/__tests__/query.test.ts
@@ -0,0 +1,34 @@
+import { createQuery } from '../query';
+import * as codec from '../codecs';
+
+describe('#createQuery', () => {
+  describe('#query', () => {
+    const params = {
+      text: ['fo o'],
+      number: [2],
+      boolean: [false],
+      many_numbers: [1, 2, 3],
+    };
+
+    const query = createQuery({
+      text: codec.string,
+      number: codec.number,
+      boolean: codec.boolean,
+      many_numbers: codec.number,
+    });
+
+    it('encodes search query from params', () => {
+      expect(query.build(params)).toEqual(
+        'text=fo+o&number=2&boolean=0&many_numbers=1&many_numbers=2&many_numbers=3',
+      );
+    });
+
+    it.skip('creates typed params from URL', () => {
+      expect(
+        query.parse(
+          'text=fo+o&number=2&boolean=0&many_numbers=1&many_numbers=2&many_numbers=3',
+        ),
+      ).toEqual(params);
+    });
+  });
+});
---
+++ src/query.ts
@@ -0,0 +1,49 @@
+import { Codec } from './codec';
+
+function parseSearch(keys: string[], search: string) {
+  const params = new URLSearchParams(search);
+  const data: Record<string, string[]> = {};
+  for (const key of keys) {
+    data[key] = params.getAll(key);
+  }
+  return data;
+}
+
+function buildSearch(data: Record<string, string[]>) {
+  const params = new URLSearchParams();
+  for (const key of Object.keys(data)) {
+    for (const value of data[key]) {
+      params.append(key, value);
+    }
+  }
+  return params.toString();
+}
+
+export type QueryCodec = Record<string, Codec<any>>;
+
+export type Values<Codec extends QueryCodec> = {
+  [key in keyof Codec]: ReturnType<Codec[key]['decode']>[];
+};
+
+export function createQuery<T extends QueryCodec>(codecs: T) {
+  const keys = Object.keys(codecs);
+
+  return {
+    parse(search: string) {
+      const source = parseSearch(keys, search);
+      const drain: Record<string, unknown[]> = {};
+      for (const key of keys) {
+        drain[key] = source[key].map(codecs[key].decode);
+      }
+      return drain as Values<T>;
+    },
+
+    build(source: Values<T>) {
+      const data: Record<string, string[]> = {};
+      for (const key of keys) {
+        data[key] = source[key].map(codecs[key].encode);
+      }
+      return buildSearch(data);
+    },
+  };
+}
