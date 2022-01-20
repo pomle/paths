5f6571c582cd22e7140c2386b6d5110c6adf0a9d
Pontus Persson <pontus@cur8.co>
2022-01-20 14:47:55 +0100

    Expose buildQuery and parseQuery functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import type { Path, PathCodec } from './path';
-import type { Query, QueryCodec } from './query';
+import type { buildQuery, parseQuery, Query, QueryCodec } from './query';
 import { Codec, createCodec } from './codec';
 import { createPath } from './path';
 import { createQuery } from './query';
 import * as codecs from './codecs';
 
 export type { Query, Path, Codec, QueryCodec, PathCodec };
-export { createCodec, createPath, createQuery, codecs };
+export { createCodec, createPath, createQuery, codecs, buildQuery, parseQuery };
