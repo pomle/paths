11995fe50fbd60702212116958afcab7b1d2282a
Pontus Alexander <pontus.alexander@gmail.com>
2024-02-21 09:19:11 +0100

    Add oneOf codec

--- src/__tests__/codecs.test.ts
+++ src/__tests__/codecs.test.ts
@@ -42,4 +42,146 @@ describe('Codecs', () => {
       expect(codecs.boolean.decode('0')).toStrictEqual(false);
     });
   });
+
+  describe('oneOf codec', () => {
+    it('throws if two options has the same string representation', () => {
+      expect(() => {
+        codecs.oneOf([1, 1] as const);
+      }).toThrow(new Error('Option to oneOf collision on key: 1'));
+
+      expect(() => {
+        codecs.oneOf([
+          {
+            toString() {
+              return 'foobar';
+            },
+          },
+          {
+            toString() {
+              return 'foobar';
+            },
+          },
+        ] as const);
+      }).toThrow(new Error('Option to oneOf collision on key: foobar'));
+    });
+
+    describe('using number', () => {
+      const states = [1, 2, 3] as const;
+      const state = codecs.oneOf(states);
+
+      it('encodes', () => {
+        expect(state.encode(2)).toStrictEqual('2');
+      });
+
+      describe('decode', () => {
+        it('decodes', () => {
+          expect(state.decode('2')).toStrictEqual(2);
+        });
+
+        it('returns first option if no match', () => {
+          expect(state.decode('miss')).toStrictEqual(1);
+        });
+      });
+    });
+
+    describe('using string', () => {
+      const states = ['low 1', 'medium 2', 'high 3'] as const;
+      const state = codecs.oneOf(states);
+
+      it('encodes', () => {
+        expect(state.encode('high 3')).toStrictEqual('high 3');
+      });
+
+      describe('decode', () => {
+        it('decodes', () => {
+          expect(state.decode('high 3')).toStrictEqual('high 3');
+        });
+
+        it('returns first option if no match', () => {
+          expect(state.decode('miss')).toStrictEqual('low 1');
+        });
+      });
+    });
+
+    describe('using standard enum', () => {
+      enum State {
+        Low,
+        Medium,
+        High,
+      }
+
+      const states = [State.Low, State.Medium, State.High] as const;
+      const state = codecs.oneOf(states);
+
+      it('encodes', () => {
+        expect(state.encode(State.High)).toStrictEqual('2');
+      });
+
+      describe('decode', () => {
+        it('decodes', () => {
+          expect(state.decode('2')).toStrictEqual(State.High);
+        });
+
+        it('returns first option if no match', () => {
+          expect(state.decode('miss')).toStrictEqual(State.Low);
+        });
+      });
+    });
+
+    describe('using string enum', () => {
+      enum State {
+        Low = 'low',
+        Medium = 'medium',
+        High = 'high',
+      }
+
+      const states = [State.Low, State.Medium, State.High] as const;
+      const state = codecs.oneOf(states);
+
+      it('encodes', () => {
+        expect(state.encode(State.High)).toStrictEqual('high');
+      });
+
+      describe('decode', () => {
+        it('decodes', () => {
+          expect(state.decode('high')).toStrictEqual(State.High);
+        });
+
+        it('returns first option if no match', () => {
+          expect(state.decode('miss')).toStrictEqual(State.Low);
+        });
+      });
+    });
+
+    describe('using custom type', () => {
+      const MyTypeOne = {
+        toString() {
+          return 'one';
+        },
+      };
+
+      const MyTypeTwo = {
+        toString() {
+          return 'two';
+        },
+      };
+
+      const states = [MyTypeOne, MyTypeTwo] as const;
+      const state = codecs.oneOf(states);
+
+      it('encodes', () => {
+        expect(state.encode(MyTypeOne)).toStrictEqual('one');
+      });
+
+      describe('decode', () => {
+        it('decodes', () => {
+          expect(state.decode('two')).toStrictEqual(MyTypeTwo);
+        });
+
+        it('returns first option if no match', () => {
+          expect(state.decode('miss')).toStrictEqual(MyTypeOne);
+        });
+      });
+    });
+  });
 });
